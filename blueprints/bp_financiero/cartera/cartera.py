c45db97414042162be17426967359046b0746b38
Manolo <ms.giraldo01@ciaf.edu.co>
2026-05-19 22:45:43 -0500

    avance cartera 19 05

--- blueprints/bp_financiero/cartera/cartera.py
+++ blueprints/bp_financiero/cartera/cartera.py
@@ -12,3 +12,37 @@ def cartera():
     return render_template(
         "financiero/cartera.html"
     )
+from flask import Blueprint, render_template
+
+bp_financiero_cartera = Blueprint(
+    "bp_financiero_cartera",
+    __name__,
+    url_prefix="/financiero/cartera"
+)
+
+
+# =========================================
+# BANDEJA CARTERA
+# =========================================
+@bp_financiero_cartera.route("/")
+def cartera():
+
+    return render_template(
+        "financiero/cartera.html"
+    )
+
+
+# =========================================
+# DETALLE CARTERA
+# =========================================
+@bp_financiero_cartera.route("/detalle/<factura>")
+def cartera_detalle(factura):
+
+    data = {
+        "numero_factura": factura
+    }
+
+    return render_template(
+        "financiero/cartera_detalle.html",
+        data=data
+    )
